98c2df9a1cd5590a4885b6e8ad242b47f875d64a
Helton Silveira <helton@heltonsouza.com.br>
2021-12-24 12:22:41 -0300

    fix: add lib for version npm

---
+++ lib/esm/index.d.ts
@@ -0,0 +1,4 @@
+import Browser from "./services/browser/index";
+import OperationSystem from "./services/os/index";
+import User from "./services/user/index";
+export { Browser, OperationSystem, User };
